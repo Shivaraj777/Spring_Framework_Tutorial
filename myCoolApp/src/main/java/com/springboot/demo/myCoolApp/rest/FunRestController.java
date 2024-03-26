114c5959bdec48be7bdc19d6e28db24bce4bab44
Shivaraj M Shetty <shivaraj916462@gmail.com>
2024-03-26 13:47:53 +0530

    creating a REST controoler in Spring

---
+++ myCoolApp/src/main/java/com/springboot/demo/myCoolApp/rest/FunRestController.java
@@ -0,0 +1,14 @@
+package com.springboot.demo.myCoolApp.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+// Create a basic REST controller to display "Hello World"
+@RestController
+public class FunRestController {
+    // expose an "/" endpoint which returns "Hello World"
+    @GetMapping("/")
+    public String sayHello(){
+        return "Hello World";
+    }
+}
