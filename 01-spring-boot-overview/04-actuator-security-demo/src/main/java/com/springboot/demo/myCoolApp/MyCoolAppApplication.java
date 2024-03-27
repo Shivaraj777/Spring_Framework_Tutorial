ed7a70d9bde4ebc8846bb299a5ba638439288bd2
Shivaraj M Shetty <shivaraj916462@gmail.com>
2024-03-27 12:52:01 +0530

    spring boot actuator security

---
+++ 01-spring-boot-overview/04-actuator-security-demo/src/main/java/com/springboot/demo/myCoolApp/MyCoolAppApplication.java
@@ -0,0 +1,14 @@
+package com.springboot.demo.myCoolApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCoolAppApplication {
+
+	public static void main(String[] args) {
+		// start/run the spring application
+		SpringApplication.run(MyCoolAppApplication.class, args);
+	}
+
+}
