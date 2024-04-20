1f4d010f52485952e2381d858810e42b5cd255c3
Shivaraj M Shetty <shivaraj916462@gmail.com>
2024-04-20 11:47:56 +0530

    Spring REST - REST path variables in api url

--- 04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/codeslayer/demo/rest/StudentRestController.java
+++ 04-spring-boot-rest-crud/01-spring-boot-rest-crud/src/main/java/com/codeslayer/demo/rest/StudentRestController.java
@@ -1,7 +1,9 @@
 package com.codeslayer.demo.rest;
 
 import com.codeslayer.demo.entity.Student;
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,14 +13,30 @@ import java.util.List;
 @RestController  // define the class as a controller
 @RequestMapping("/api")
 public class StudentRestController {
+    List<Student> theStudents;
+
+    // define Post construct method to create the student data only once when bean is created
+    @PostConstruct
+    private void loadStudentsData(){
+        theStudents = new ArrayList<>();
+        theStudents.add(new Student("Kiyotaka", "Ayanokoji"));
+        theStudents.add(new Student("Hiyori", "Shiina"));
+        theStudents.add(new Student("Suzune", "Horikita"));
+    }
+
 
     // api to return a list of Students
+    // route - /api/students
     @GetMapping("/students")
     public List<Student> getStudents(){
-        List<Student> students = new ArrayList<>();
-        students.add(new Student("Kiyotaka", "Ayanokoji"));
-        students.add(new Student("Hiyori", "Shiina"));
-        students.add(new Student("Suzune", "Horikita"));
-        return students;
+        return theStudents;
+    }
+
+
+    // define api to get a particular student
+    // route /api/students/{studentId}
+    @GetMapping("/students/{studentId}")
+    public Student getStudent(@PathVariable int studentId){
+        return theStudents.get(studentId);
     }
 }
