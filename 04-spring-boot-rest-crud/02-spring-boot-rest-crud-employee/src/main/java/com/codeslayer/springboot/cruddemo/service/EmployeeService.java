9eddd571032fc770692a3886523c03ca6d67a248
Shivaraj M Shetty <shivaraj916462@gmail.com>
2024-04-21 18:17:35 +0530

    Spring REST - add service methods

--- 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/codeslayer/springboot/cruddemo/service/EmployeeService.java
+++ 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/codeslayer/springboot/cruddemo/service/EmployeeService.java
@@ -8,4 +8,9 @@ public interface EmployeeService {
 
     public List<Employee> findAll();  // service method to connect to employee DAO to get all employees
 
+    public Employee findById(int employeeId);  // service method declaration to connect to DAO fineById method
+
+    public Employee save(Employee theEmployee);  // service method declaration to connect to DAO save method
+
+    public void deleteById(int employeeId);  // service method declaration to connect to DAO deleteById method
 }
--- 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/codeslayer/springboot/cruddemo/service/EmployeeServiceImpl.java
+++ 04-spring-boot-rest-crud/02-spring-boot-rest-crud-employee/src/main/java/com/codeslayer/springboot/cruddemo/service/EmployeeServiceImpl.java
@@ -4,6 +4,7 @@ import com.codeslayer.springboot.cruddemo.dao.EmployeeDAO;
 import com.codeslayer.springboot.cruddemo.entity.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -23,5 +24,28 @@ public class EmployeeServiceImpl implements EmployeeService{
     @Override
     public List<Employee> findAll() {
         return employeeDAO.findAll();
+    }
+
+
+    // implement service method to connect to DAO fineById method
+    @Override
+    public Employee findById(int employeeId) {
+        return employeeDAO.findById(employeeId);
+    }
+
+
+    // implement service method to connect to DAO save method
+    @Transactional
+    @Override
+    public Employee save(Employee theEmployee) {
+        return employeeDAO.save(theEmployee);
+    }
+
+
+    // implement service method to connect to DAO deleteById method
+    @Transactional
+    @Override
+    public void deleteById(int employeeId) {
+        employeeDAO.deleteById(employeeId);
     }
 }
