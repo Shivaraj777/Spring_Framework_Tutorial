9de8dc8e0c846f688d8db686a412eb348ff78f4e
Shivaraj M Shetty <shivaraj916462@gmail.com>
2024-04-25 18:40:11 +0530

    Spring Security - securing API endpoints basics

---
+++ 05-spring-boot-rest-security/00-spring-boot-rest-security-employee-starter-code/src/main/java/com/codeslayer/springboot/cruddemo/dao/EmployeeRepository.java
@@ -0,0 +1,12 @@
+package com.codeslayer.springboot.cruddemo.dao;
+
+import com.codeslayer.springboot.cruddemo.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+    // Employee -> Entity Type
+    // Integer -> Primary key type
+
+    // no need to write any implementation class code...
+    // use the super class built-in methods by creating a reference of EmployeeRepository
+}
