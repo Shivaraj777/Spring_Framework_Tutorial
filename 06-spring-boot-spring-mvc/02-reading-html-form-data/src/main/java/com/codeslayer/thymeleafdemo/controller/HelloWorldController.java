38243a265f4765cb177a1989dafc5a1a5d2251a7
Shivaraj M Shetty <shivaraj916462@gmail.com>
2024-04-29 18:39:27 +0530

    Spring MVC - adding request data to spring MVC model and reading it

--- 06-spring-boot-spring-mvc/02-reading-html-form-data/src/main/java/com/codeslayer/thymeleafdemo/controller/HelloWorldController.java
+++ 06-spring-boot-spring-mvc/02-reading-html-form-data/src/main/java/com/codeslayer/thymeleafdemo/controller/HelloWorldController.java
@@ -1,5 +1,6 @@
 package com.codeslayer.thymeleafdemo.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,9 +16,26 @@ public class HelloWorldController {
         return "helloWorld-form";
     }
 
+
     // controller method to process HTML form
     @GetMapping("/processForm")
     public String processForm(){
         return "helloWorld";
     }
+
+
+    // controller method to read form data and adding it to model
+    // request -> to read and manipulate request data
+    @GetMapping("processFormVersion2")
+    public String processFormV2(HttpServletRequest request, Model theModel){
+        // get form data from request and process it
+        String theName = request.getParameter("studentName");
+        theName.toUpperCase();
+        String result = "Yo " + theName;
+
+        // add the processed form data to spring MVC model
+        theModel.addAttribute("message", result);
+        return "helloWorld";
+    }
+
 }
