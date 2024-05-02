85f90a05049712d6abb9c33f3ebc5c63c58aa28d
Shivaraj M Shetty <shivaraj916462@gmail.com>
2024-05-02 14:13:07 +0530

    spring MVC - adding validation ranges

--- 06-spring-boot-spring-mvc/03-spring-mvc-form-validation-demo/src/main/java/com/codeslayer/springdemo/mvc/model/Customer.java
+++ 06-spring-boot-spring-mvc/03-spring-mvc-form-validation-demo/src/main/java/com/codeslayer/springdemo/mvc/model/Customer.java
@@ -1,5 +1,7 @@
 package com.codeslayer.springdemo.mvc.model;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
@@ -10,6 +12,11 @@ public class Customer {
     @NotNull(message = "is required")  // @NotNull() -> attribute value should not be null
     @Size(min = 1, message = "is required")  // min size of attribute value should be 1
     private String lastName;
+
+    // @Max and @Min annotations are used to set the min and max value for the attribute
+    @Min(value = 0, message = "value must be greater than or equal to 0")
+    @Max(value = 10, message = "value must be less than or equal to 10")
+    private int freePasses;
 
     public Customer() { }
 
@@ -27,5 +34,13 @@ public class Customer {
 
     public void setLastName(String lastName) {
         this.lastName = lastName;
+    }
+
+    public int getFreePasses() {
+        return freePasses;
+    }
+
+    public void setFreePasses(int freePasses) {
+        this.freePasses = freePasses;
     }
 }
