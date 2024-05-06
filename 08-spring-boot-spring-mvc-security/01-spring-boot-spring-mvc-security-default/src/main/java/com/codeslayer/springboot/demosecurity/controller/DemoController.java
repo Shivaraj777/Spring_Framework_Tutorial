4ccd9781f14bd8b2b2d51afcee58d7a0ab7689be
Shivaraj M Shetty <shivaraj916462@gmail.com>
2024-05-06 13:46:33 +0530

    spring mvc security - project setup

---
+++ 08-spring-boot-spring-mvc-security/01-spring-boot-spring-mvc-security-default/src/main/java/com/codeslayer/springboot/demosecurity/controller/DemoController.java
@@ -0,0 +1,15 @@
+package com.codeslayer.springboot.demosecurity.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/demo")
+public class DemoController {
+
+    @GetMapping("/")
+    public String showHome(){
+        return "home";
+    }
+}
