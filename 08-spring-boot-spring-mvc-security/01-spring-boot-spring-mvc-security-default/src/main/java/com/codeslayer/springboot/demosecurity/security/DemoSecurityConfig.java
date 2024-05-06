190803b323ee702918a75879c7880c2b1014ac1f
Shivaraj M Shetty <shivaraj916462@gmail.com>
2024-05-06 14:22:21 +0530

    spring mvc security - creating users in memory using security config class

---
+++ 08-spring-boot-spring-mvc-security/01-spring-boot-spring-mvc-security-default/src/main/java/com/codeslayer/springboot/demosecurity/security/DemoSecurityConfig.java
@@ -0,0 +1,36 @@
+package com.codeslayer.springboot.demosecurity.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+// spring security configuration class
+@Configuration
+public class DemoSecurityConfig {
+
+    // method to create users in-memory
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager(){
+        UserDetails john = User.builder()
+                .username("John")
+                .password("{noop}test123")
+                .roles("EMPLOYEE")
+                .build();
+
+        UserDetails mary = User.builder()
+                .username("Mary")
+                .password(("{noop}test123"))
+                .roles("EMPLOYEE", "MANAGER")
+                .build();
+
+        UserDetails susan = User.builder()
+                .username("Susan")
+                .password("{noop}test123")
+                .roles("EMPLOYEE", "MANAGER", "ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(john, mary, susan);
+    }
+}
