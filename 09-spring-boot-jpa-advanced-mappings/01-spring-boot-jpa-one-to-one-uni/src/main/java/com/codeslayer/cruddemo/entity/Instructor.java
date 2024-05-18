40e2366fc16748caabb2024c7a095f53c052c69a
Shivaraj M Shetty <shivaraj916462@gmail.com>
2024-05-18 12:17:54 +0530

    spring jdbc one-to-one mapping - create entities and add relationship

---
+++ 09-spring-boot-jpa-advanced-mappings/01-spring-boot-jpa-one-to-one-uni/src/main/java/com/codeslayer/cruddemo/entity/Instructor.java
@@ -0,0 +1,94 @@
+package com.codeslayer.cruddemo.entity;
+
+import jakarta.persistence.*;
+import org.springframework.boot.autoconfigure.web.WebProperties;
+
+@Entity
+@Table(name = "instructor")
+public class Instructor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_Name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+    /*
+        @OneToOne annotation -> create a one-to-one relationship mapping
+        between instructor and instructor_detail table...
+        CascadeType.ALL -> updates instructor_detail table for all operations performed on instructor table
+        @JoinColumn -> to denote a foreign key in instructor which references to instructor_detail id
+
+    */
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "entity_detail_id")
+    private InstructorDetail instructorDetail;
+
+
+    public Instructor() { }
+
+    public Instructor(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public InstructorDetail getInstructorDetail() {
+        return instructorDetail;
+    }
+
+    public void setInstructorDetail(InstructorDetail instructorDetail) {
+        this.instructorDetail = instructorDetail;
+    }
+
+    @Override
+    public String toString() {
+        return "Instructor{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", instructorDetail=" + instructorDetail +
+                '}';
+    }
+}
