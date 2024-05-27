8e7a209946446dc92ca4bc225bb666dcf1b3e304
Shivaraj M Shetty <shivaraj916462@gmail.com>
2024-05-27 16:51:04 +0530

    spring jdbc one-to-many-uni mapping - initial setup

---
+++ 09-spring-boot-jpa-advanced-mappings/04-spring-boot-jpa-one-to-many-uni/src/main/java/com/codeslayer/cruddemo/CruddemoApplication.java
@@ -0,0 +1,219 @@
+package com.codeslayer.cruddemo;
+
+import com.codeslayer.cruddemo.dao.AppDAO;
+import com.codeslayer.cruddemo.entity.Course;
+import com.codeslayer.cruddemo.entity.Instructor;
+import com.codeslayer.cruddemo.entity.InstructorDetail;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
+
+@SpringBootApplication
+public class CruddemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CruddemoApplication.class, args);
+	}
+
+
+	/*
+		Command line function -> displays output on command line
+		from spring boot framework... it is executed after the spring beans are loaded
+		AppDAO is injected automatically -> because the method is annotated with @Bean
+	*/
+	@Bean
+	public CommandLineRunner commandLineRunner(AppDAO appDAO){
+		// lambda expression - shorthand expression for creating an implementation for command runner
+		return runner -> {
+			// System.out.println("Hello World!");
+			// createInstructor(appDAO);
+			// findInstructor(appDAO);
+			// deleteInstructor(appDAO);
+			// findInstructorDetail(appDAO);
+			// deleteInstructorDetail(appDAO);
+			// createInstructorWithCourses(appDAO);
+			// findInstructorWithCourses(appDAO);
+			// findCoursesForInstructor(appDAO);
+			// findInstructorWithCoursesJoinFetch(appDAO);
+			// updateInstructor(appDAO);
+			// updateCourse(appDAO);
+			deleteCourse(appDAO);
+		};
+	}
+
+
+	// method to delete a Course by connecting to DAO
+	private void deleteCourse(AppDAO appDAO) {
+		int courseId = 11;
+		System.out.println("Deleting course with id: " + courseId);
+		appDAO.deleteCourseById(courseId);
+		System.out.println("Done");
+	}
+
+
+	// method to update Course by connecting to DAO
+	private void updateCourse(AppDAO appDAO) {
+		// find the course
+		int courseId = 10;
+		System.out.println("Finding the course with id: " + courseId);
+		Course tempCourse = appDAO.findCourseById(courseId);
+
+		// update the instructor
+		System.out.println("Updating the course with id: " + courseId);
+		tempCourse.setTitle("Finding Inner Peace - Master Class");
+		appDAO.update(tempCourse);
+		System.out.println("Done");
+	}
+
+
+	// method to update instructor by connecting to DAO
+	private void updateInstructor(AppDAO appDAO) {
+		// find the instructor
+		int instructorId = 1;
+		System.out.println("Finding the instructor with id: " + instructorId);
+		Instructor tempInstructor = appDAO.findInstructorById(instructorId);
+
+		// update the instructor
+		System.out.println("Updating the instructor with id: " + instructorId);
+		tempInstructor.setLastName("Darby");
+		appDAO.update(tempInstructor);
+		System.out.println("Done");
+	}
+
+	// method to find instructor and associated course with JOIN FETCH query by connecting to DAO
+	private void findInstructorWithCoursesJoinFetch(AppDAO appDAO) {
+		// find instructor and associated courses
+		int instructorId = 1;
+		System.out.println("Finding instructor and associated courses with id: " + instructorId);
+		Instructor tempInstructor = appDAO.findInstructorByIdJoinFetch(instructorId);
+
+		// print the data
+		System.out.println("Instructor: " + tempInstructor);
+		System.out.println("Associated courses: " + tempInstructor.getCourses());
+		System.out.println("Done");
+	}
+
+
+	// method to find the list of associated courses for an instructor bu connecting to DAO
+	private void findCoursesForInstructor(AppDAO appDAO) {
+		// find the instructor
+		int instructorId = 1;
+		System.out.println("Finding the instructor with id: " + instructorId);
+		Instructor tempInstructor = appDAO.findInstructorById(instructorId);
+		System.out.println("Instructor: " + tempInstructor);  // only instructor details will be loaded as fetch type is LAZY
+
+		// find the associated courses for instructor and add it to instructor
+		System.out.println("Finding courses for instructor id: " + instructorId);
+		List<Course> tempCourses = appDAO.findCoursesByInstructorId(instructorId);
+		tempInstructor.setCourses(tempCourses);
+		System.out.println("The associated courses for Instructor: " + tempInstructor.getCourses());
+		System.out.println("Done");
+	}
+
+
+	// method to find instructor and associated course
+	// eager vs lazy load example overview -> we get error when we try to access courses as default fetch type is LAZY for @OneToMany mapping
+	private void findInstructorWithCourses(AppDAO appDAO) {
+		// find the Instructor
+		int instructorId = 1;
+		System.out.println("Finding instructor id: " + instructorId);
+		Instructor tempInstructor = appDAO.findInstructorById(instructorId);
+
+		// print instructor and associated courses
+		System.out.println("Instructor: " + tempInstructor);
+		System.out.println("The associated courses " + tempInstructor.getCourses());
+		System.out.println("Done!");
+	}
+
+
+	// method to created instructor and associated courses
+	private void createInstructorWithCourses(AppDAO appDAO) {
+		// create the instructor
+		Instructor tempInstructor = new Instructor("Rock", "Dickson", "dickson@gmail.com");
+
+		// create instructor detail
+		InstructorDetail tempInstructorDetail = new InstructorDetail("http://www.luv2code.com/youtube", "Luv 2 code!!");
+
+		// associate the objects
+		tempInstructor.setInstructorDetail(tempInstructorDetail);
+
+		// create some course
+		Course tempCourse1 = new Course("Air Guitar - The Ultimate Guide");
+		Course tempCourse2 = new Course("The Pinball - Masterclass");
+
+		// add the courses to the instructor
+		tempInstructor.addCourse(tempCourse1);
+		tempInstructor.addCourse(tempCourse2);
+
+		// save the instructor
+		System.out.println("Saving Instructor: " + tempInstructor);
+		System.out.println("The Course: " + tempInstructor.getCourses());
+		appDAO.save(tempInstructor);
+		System.out.println("Done!");
+	}
+
+
+	// method to delete instructor details by connecting to DAO
+	private void deleteInstructorDetail(AppDAO appDAO) {
+		int instructorDetailId = 3;
+		System.out.println("Deleting the instructor detail with id: " + instructorDetailId);
+		appDAO.deleteInstructorDetailById(instructorDetailId);
+		System.out.println("Done!!");
+	}
+
+
+	// method to find instructor detail by connecting to DAO
+	private void findInstructorDetail(AppDAO appDAO) {
+		// get the instructor detail object
+		int instructorDetailId = 2;
+		System.out.println("Finding instructor detail with id: " + instructorDetailId);
+		InstructorDetail tempInstructorDetail = appDAO.findInstructorDetailById(instructorDetailId);
+
+		// print instructor detail and associate instructor
+		System.out.println("Instructor Detail: " + tempInstructorDetail);
+		System.out.println("Associated Instructor: " + tempInstructorDetail.getInstructor());
+	}
+
+
+	// method to delete instructor by id by connecting to DAO
+	private void deleteInstructor(AppDAO appDAO) {
+		int instructorId = 1;
+		System.out.println("Deleting the instructor with id: " + instructorId);
+		appDAO.deleteInstructorById(instructorId);
+		System.out.println("Done!!");
+	}
+
+
+	// method to find instructor by id by connecting to DAO
+	private void findInstructor(AppDAO appDAO) {
+		// find the instructor
+		int instructorId = 1;
+		System.out.println("Finding the instructor with id: " + instructorId);
+		Instructor tempInstructor = appDAO.findInstructorById(instructorId);
+
+		System.out.println("Instructor: " + tempInstructor);
+		System.out.println("The associated instructor details: " + tempInstructor.getInstructorDetail());
+	}
+
+
+	// method to create instructor by connecting to DAO
+	private void createInstructor(AppDAO appDAO) {
+		// create the instructor
+		Instructor tempInstructor = new Instructor("Rock", "Dickson", "dickson@gmail.com");
+
+		// create instructor detail
+		InstructorDetail tempInstructorDetail = new InstructorDetail("http://www.luv2code.com/youtube", "Luv 2 code!!");
+
+		// associate the objects
+		tempInstructor.setInstructorDetail(tempInstructorDetail);
+
+		// save the instructor
+		// Note: this will also save instructor_details -> why?... CascadeType.ALL
+		System.out.println("Saving instructor: " + tempInstructor);
+		appDAO.save(tempInstructor);
+	}
+
+}
---
+++ 09-spring-boot-jpa-advanced-mappings/04-spring-boot-jpa-one-to-many-uni/src/main/java/com/codeslayer/cruddemo/dao/AppDAOImpl.java
@@ -0,0 +1,143 @@
+package com.codeslayer.cruddemo.dao;
+
+import com.codeslayer.cruddemo.entity.Course;
+import com.codeslayer.cruddemo.entity.Instructor;
+import com.codeslayer.cruddemo.entity.InstructorDetail;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Repository  // to include DAO for component scanning
+public class AppDAOImpl implements AppDAO{
+
+    // define entity manager field to connect to database
+    private EntityManager entityManager;
+
+    // inject EntityManager object using constructor injection
+    @Autowired
+    public AppDAOImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+
+    // method to save the instructor and instructor details in database
+    @Override
+    @Transactional
+    public void save(Instructor theInstructor) {
+        // saves the instructor and instructor detail data in database -> why?... -> CascadeType = ALL
+        entityManager.persist(theInstructor);
+    }
+
+
+    // method to find the instructor by id
+    @Override
+    public Instructor findInstructorById(int instructorId) {
+        // associated instructor details data is also retrieved, as the default behaviour of @OneToOne fetch type is eager
+        return entityManager.find(Instructor.class, instructorId);
+    }
+
+
+    // method to delete instructor by id
+    @Override
+    @Transactional
+    public void deleteInstructorById(int instructorId) {
+        // retrieve the instructor
+        Instructor tempInstructor = entityManager.find(Instructor.class, instructorId);
+
+        // retrieve associated course
+        List<Course> tempCourses = tempInstructor.getCourses();
+
+        // break the association/references for all the courses which instructor is mapped to
+        for(Course tempCourse : tempCourses){
+            tempCourse.setInstructor(null);
+        }
+
+        // delete instructor(deletes both instructor and associated instructor details)
+        entityManager.remove(tempInstructor);
+    }
+
+
+    // method to find instructor-detail by id
+    @Override
+    public InstructorDetail findInstructorDetailById(int instructorDetailId) {
+        return entityManager.find(InstructorDetail.class, instructorDetailId);
+    }
+
+
+    // method to delete instructor-detail by id
+    @Override
+    @Transactional
+    public void deleteInstructorDetailById(int instructorDetailId) {
+        // retrieve the instructorDetail and delete it(deletes both instructor details and associated instructor)
+        InstructorDetail tempInstructorDetail = entityManager.find(InstructorDetail.class, instructorDetailId);
+        entityManager.remove(tempInstructorDetail);
+    }
+
+
+    // method to find the list of associated courses for an instructor
+    @Override
+    public List<Course> findCoursesByInstructorId(int instructorId) {
+        // create the query and set the parameter
+        TypedQuery<Course> query = entityManager.createQuery("from Course where instructor.id = :data", Course.class);
+        query.setParameter("data", instructorId);
+
+        // execute the query
+        List<Course> tempCourse = query.getResultList();
+        return tempCourse;
+    }
+
+
+    // method to find instructor and its associated courses(using JOIN FETCH)
+    // JOIN FETCH loads the data in an EAGER fashion even if Fetch Type of entity is LAZY
+    // the below code will return Instructor and associated courses embedded
+    @Override
+    public Instructor findInstructorByIdJoinFetch(int instructorId) {
+        // create the query to find instructor with JOIN FETCH
+        TypedQuery<Instructor> query = entityManager.createQuery("select i from Instructor i " +
+                                                                "JOIN FETCH i.courses " +
+                                                                "JOIN FETCH i.instructorDetail " +
+                                                                "where i.id = :data", Instructor.class);
+
+        query.setParameter("data", instructorId);
+
+        // execute the query and return the result
+        Instructor tempInstructor = query.getSingleResult();
+        return tempInstructor;
+    }
+
+
+    // method to update instructor
+    @Override
+    @Transactional
+    public void update(Instructor theInstructor) {
+        entityManager.merge(theInstructor);
+    }
+
+
+    @Override
+    public Course findCourseById(int courseId) {
+        return entityManager.find(Course.class, courseId);
+    }
+
+
+    // method to delete a course based on id
+    @Override
+    @Transactional
+    public void deleteCourseById(int courseId) {
+        // retrieve the course and delete it
+        Course tempCourse = entityManager.find(Course.class, courseId);
+        entityManager.remove(tempCourse);
+    }
+
+
+    @Override
+    @Transactional
+    public void update(Course theCourse) {
+        entityManager.merge(theCourse);
+    }
+
+}
