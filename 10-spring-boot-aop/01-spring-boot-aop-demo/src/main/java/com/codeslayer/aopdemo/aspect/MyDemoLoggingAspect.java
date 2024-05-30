b49419a186c74a19ec230142f7089debdd0c3446
Shivaraj M Shetty <shivaraj916462@gmail.com>
2024-05-30 16:59:12 +0530

    spring aop - pointcut expression, match method with any params

--- 10-spring-boot-aop/01-spring-boot-aop-demo/src/main/java/com/codeslayer/aopdemo/aspect/MyDemoLoggingAspect.java
+++ 10-spring-boot-aop/01-spring-boot-aop-demo/src/main/java/com/codeslayer/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -31,12 +31,14 @@ public class MyDemoLoggingAspect {
         "execution(* add*(com.codeslayer.aopdemo.entity.Account))"  -> match methods with as single Account object as parameter
         "execution(* add*(com.codeslayer.aopdemo.entity.Account, ..))"  ->  match methods with Account object param and any no. of arguments
         ..  ---> specifies we can pass any no. of arguments to the function
+        "execution(* add*(..))"  ->  match methods with zero or any parameters
      */
     // @Before("execution(public void addAccount())")
     // @Before("execution(public void com.codeslayer.aopdemo.dao.AccountDAO.addAccount())")
     // @Before("execution(public void add*())")
     // @Before("execution(* add*(com.codeslayer.aopdemo.entity.Account))")
-    @Before("execution(* add*(com.codeslayer.aopdemo.entity.Account, ..))")
+    // @Before("execution(* add*(com.codeslayer.aopdemo.entity.Account, ..))")
+    @Before("execution(* add*(..))")
     public void beforeAddAccountAdvice(){
         System.out.println("\n========> Executing @Before Advice on method");
     }
