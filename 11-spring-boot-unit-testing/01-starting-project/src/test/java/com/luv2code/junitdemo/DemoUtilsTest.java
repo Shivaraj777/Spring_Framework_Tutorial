a47f0a61a5635c2300ecf4420253b8a4b1211993
Shivaraj M Shetty <shivaraj916462@gmail.com>
2024-06-11 22:59:39 +0530

    junit - test case lifecycle methods

--- 11-spring-boot-unit-testing/01-starting-project/src/test/java/com/luv2code/junitdemo/DemoUtilsTest.java
+++ 11-spring-boot-unit-testing/01-starting-project/src/test/java/com/luv2code/junitdemo/DemoUtilsTest.java
@@ -1,7 +1,6 @@
 package com.luv2code.junitdemo;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
@@ -9,13 +8,39 @@ import static org.junit.jupiter.api.Assertions.assertNotEquals;
 /*
     test classes are not public in general
     @Test annotation is used to create a unit test case
+    By default @BeforeAll and @AfterAll methods must be static
 */
 class DemoUtilsTest {
+
+    DemoUtils demoUtils;
+
+    @BeforeAll
+    static void setupBeforeAll(){
+        System.out.println("@BeforeAll executes only once before all test methods");
+    }
+
+    @BeforeEach
+    void setupBeforeEach(){
+        demoUtils = new DemoUtils();
+        System.out.println("\n@BeforeEach executes before the execution of each method");
+    }
+
+
+    @AfterAll
+    static void setupAfterAll(){
+        System.out.println("\n@AfterAll executes only once after all test methods");
+    }
+
+
+    @AfterEach
+    void tearDownAfterEach(){
+        System.out.println("@AfterEach executes after the execution of each method");
+    }
 
     // unit test to check if arithmetic result is equal/not-equal
     @Test
     void testEqualsAndNotEquals(){
-        DemoUtils demoUtils = new DemoUtils();
+        System.out.println("Running test: testEqualsAndNotEquals");
         assertEquals(6, demoUtils.add(2, 4), "2 + 4 must be 6");
         assertNotEquals(6, demoUtils.add(1, 9), "1 + 9 must not be 6");
     }
@@ -24,7 +49,7 @@ class DemoUtilsTest {
     // unit test to check if an object is null/not null
     @Test
     void testNullAndNotNull(){
-        DemoUtils demoUtils = new DemoUtils();
+        System.out.println("Running test: testNullAndNotNull");
         String s1 = null;
         String s2 = "hello";
         Assertions.assertNull(s1, "Object should be null!");
