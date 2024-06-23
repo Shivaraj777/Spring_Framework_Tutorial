2e78662a8d05ad83b82ab0cd0af87bc037747618
Shivaraj M Shetty <shivaraj916462@gmail.com>
2024-06-23 13:19:49 +0530

    junit - conditional tests - more Conditional annotations

--- 11-spring-boot-unit-testing/03-spring-boot-junit-code-coverage-maven/src/test/java/com/luv2code/junitdemo/ConditionalTest.java
+++ 11-spring-boot-unit-testing/03-spring-boot-junit-code-coverage-maven/src/test/java/com/luv2code/junitdemo/ConditionalTest.java
@@ -2,14 +2,16 @@ package com.luv2code.junitdemo;
 
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.condition.DisabledOnOs;
-import org.junit.jupiter.api.condition.EnabledOnOs;
-import org.junit.jupiter.api.condition.OS;
+import org.junit.jupiter.api.condition.*;
 
 
 /*
     @Disabled annotation denotes that test method should not be executed
-    @EnabledOnOS annotation is used to execute tests on specified Operating systems
+    @EnabledOnOs annotation is used to execute tests on specified Operating systems
+    @EnabledOnJre annotation is sued to execute tests on specified versions of JDK/JRE
+    @EnabledForJreRange annotation is used to execute tests if JRE version lies between a specified range
+    @EnabledIfSystemProperty annotation is used to execute tests if a certain system property is present in test configuration
+    @EnabledIfEnvironmentVariable annotation is used to execute tests if a certain environment variable is present in test configuration
 */
 public class ConditionalTest {
 
@@ -44,6 +46,55 @@ public class ConditionalTest {
     @Test
     @EnabledOnOs({ OS.WINDOWS, OS.MAC })
     void testForMacAndWindowsOnly(){
+        // execute method and perform asserts
+    }
+
+
+    @Test
+    @EnabledOnJre(JRE.JAVA_17)
+    void testForJRE17Only(){
+        // execute method and perform asserts
+    }
+
+
+    @Test
+    @EnabledOnJre(JRE.JAVA_21)
+    void testForJRE21Only(){
+        // execute method and perform asserts
+    }
+
+
+    @Test
+    @EnabledOnJre({ JRE.JAVA_17, JRE.JAVA_21})
+    void testForJRE17and21(){
+        // execute method and perform asserts
+    }
+
+
+    @Test
+    @EnabledForJreRange(min = JRE.JAVA_8, max = JRE.JAVA_17)
+    void testForJRERange1(){
+        // execute method and perform asserts
+    }
+
+
+    @Test
+    @EnabledForJreRange(min = JRE.JAVA_8, max = JRE.JAVA_21)
+    void testForJRERange2(){
+        // execute method and perform asserts
+    }
+
+
+    @Test
+    @EnabledIfSystemProperty(named = "TEST_SYS_PROP", matches = "CI_CD_DEPLOY")
+    void testForSystemProperty(){
+        // execute method and perform asserts
+    }
+
+
+    @Test
+    @EnabledIfEnvironmentVariable(named = "TEST_ENV", matches = "DEV")
+    void testForEnvironmentVariable(){
         // execute method and perform asserts
     }
 }
