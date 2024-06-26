8ba08d9189a38f9171cc1094a4b5d0c298173ae4
Shivaraj M Shetty <shivaraj916462@gmail.com>
2024-06-26 12:20:37 +0530

    TDD - create failing unit tests and devlope code to pass tests

--- 11-spring-boot-unit-testing/04 spring-boot-tdd/src/test/java/org/codeslayer/tdd/FizzBuzzTest.java
+++ 11-spring-boot-unit-testing/04 spring-boot-tdd/src/test/java/org/codeslayer/tdd/FizzBuzzTest.java
@@ -13,6 +13,35 @@ public class FizzBuzzTest {
     @DisplayName("Divisible by Three")
     @Order(1)
     void testForDivisibleByThree(){
-        Assertions.fail("Fail");  // assertion to fail the test by default
+        // Assertions.fail("Fail");  // assertion to fail the test by default
+        String expected = "Fizz";
+        Assertions.assertEquals(expected, FizzBuzz.compute(3), "Should return Fizz");
+    }
+
+
+    @Test
+    @DisplayName("Divisible by Five")
+    @Order(2)
+    void testForDivisibleByFive(){
+        String expected = "Buzz";
+        Assertions.assertEquals(expected, FizzBuzz.compute(5), "Should return Buzz");
+    }
+
+
+    @Test
+    @DisplayName("Divisible by Three & Five")
+    @Order(3)
+    void testForDivisibleByThreeAndFive(){
+        String expected = "FizzBuzz";
+        Assertions.assertEquals(expected, FizzBuzz.compute(15), "Should return FizzBuzz");
+    }
+
+
+    @Test
+    @DisplayName("Not Divisible by Three & Five")
+    @Order(4)
+    void testForNotDivisibleByThreeAndFive(){
+        String expected = "2";
+        Assertions.assertEquals(expected, FizzBuzz.compute(2), "Should return 2");
     }
 }
