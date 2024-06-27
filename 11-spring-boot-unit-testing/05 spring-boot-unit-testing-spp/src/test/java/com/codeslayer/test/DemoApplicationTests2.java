86561d95bae108c4fadac90f6ebb3cff2c5e48be
Shivaraj M Shetty <shivaraj916462@gmail.com>
2024-06-27 15:59:29 +0530

    SB UT - inject objects and setup BeforeEach method

--- 11-spring-boot-unit-testing/05 spring-boot-unit-testing-spp/src/test/java/com/codeslayer/test/DemoApplicationTests2.java
+++ 11-spring-boot-unit-testing/05 spring-boot-unit-testing-spp/src/test/java/com/codeslayer/test/DemoApplicationTests2.java
@@ -1,8 +1,16 @@
 package com.codeslayer.test;
 
 import com.codeslayer.demo.DemoApplication;
+import com.codeslayer.demo.models.CollegeStudent;
+import com.codeslayer.demo.models.StudentGrades;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 /*
     if the Test class is in a different package from main application class,
@@ -10,6 +18,38 @@ import org.springframework.boot.test.context.SpringBootTest;
 */
 @SpringBootTest(classes = DemoApplication.class)
 public class DemoApplicationTests2 {
+
+    private static int count = 0;
+
+    @Value("${info.app.name}")
+    private String appInfo;
+
+    @Value("${info.app.description}")
+    private String appDescription;
+
+    @Value("${info.app.version}")
+    private String appVersion;
+
+    @Value("${info.school.name}")
+    private String schoolName;
+
+    @Autowired
+    CollegeStudent student;
+
+    @Autowired
+    StudentGrades studentGrades;
+
+
+    @BeforeEach
+    public void beforeEach(){
+        count++;
+        System.out.println("Testing: " + appInfo + " which is " + appDescription + " Version: " + appVersion + ". Execution of test method " + count);
+        student.setFirstname("Erica");
+        student.setLastname("Cantona");
+        student.setEmailAddress("erica.cantona@codeslayer.com");
+        studentGrades.setMathGradeResults(new ArrayList<>(Arrays.asList(100.0, 85.0, 76.50, 91.75)));
+        student.setStudentGrades(studentGrades);
+    }
 
     @Test
     void demoTest(){
